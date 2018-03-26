f1289fc978c8a3ba22d94b54dfe65957a8be8980
RedRiot <quoc.tung@outlook.com>
2018-03-26 10:01:27 +0200

    feat(wordSplitter)created method

--- 15/src/Main.java
+++ 15/src/Main.java
@@ -1,5 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(wordSplitter("ImTung"));
+        }
+
+    private static List<String> wordSplitter(String string) {
+        int partitionSize = 2;
+        List<String> parts = new ArrayList<String>();
+        int length = string.length();
+        for (int i=0; i<length; i+=partitionSize)
+        {
+            parts.add(string.substring(i, Math.min(length, i + partitionSize)));
+        }
+        return parts;
     }
 }
+
